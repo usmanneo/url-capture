84013f50ee5e6da61189ddf4b0f1159097df4c49
usmanneo <h4k3rtools@gmail.com>
2024-07-30 06:09:52 -0700

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'your_cloud_name', // replace with your Cloudinary cloud name
+  cloud_name: 'davvrbuia', // replace with your Cloudinary cloud name
   api_key: '156336359935233', // replace with your Cloudinary API key
   api_secret: 'SBecBuIwu0GA_CzfjSTEhA2WGEM' // replace with your Cloudinary API secret
 });
