e25939956986d5f813c1483ec46f7f5e675894a5
usmanneo <h4k3rtools@gmail.com>
2024-07-30 02:57:47 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,13 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-  logging: false, // Disable logging; default: console.log
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: false,
 });
 
 module.exports = sequelize;
