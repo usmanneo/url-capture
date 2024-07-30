56ebff5001509bf0b3f62da2f18c1f327a4cc7dd
usmanneo <h4k3rtools@gmail.com>
2024-07-30 02:52:45 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,30 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'sqlite',
-  logging: false,
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false, // Disable logging; default: console.log
 });
 
-const URL = sequelize.define('URL', {
-  originalUrl: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  uniqueId: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  redirectUrl: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  imageData: {
-    type: Sequelize.TEXT,
-    allowNull: true,
-  },
-});
-
-sequelize.sync();
-
-module.exports = { sequelize, URL };
+module.exports = sequelize;
